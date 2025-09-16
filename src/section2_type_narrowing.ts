2ad9c9891ad3c0a7e112f3ebbbf3f219fc932597
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-16 11:37:59 -0400

    Write function printValue

--- src/section2_type_narrowing.ts
+++ src/section2_type_narrowing.ts
@@ -8,4 +8,12 @@
 
 // Your code here 👇
 
-function printValue 
+const printValue = (value: string | number | boolean): void => {
+  if (typeof value === "string") {
+    console.log(`String value: ${value}`)
+  } else if (typeof value === "number") {
+    console.log(`Number value: ${value}`)
+  } else {
+    console.log(`Boolean value: ${value}`)
+  }
+}
