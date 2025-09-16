ef523740d424091bbc0fbeacf277793604a1db0b
Mike <mclancy96@gmail.com>
2025-09-16 15:54:29 -0400

    103

--- src/section2_type_narrowing.ts
+++ src/section2_type_narrowing.ts
@@ -7,3 +7,12 @@
 // - boolean: print "Boolean value: <value>"
 
 // Your code here 👇
+const printValue = (value: string | number | boolean): void => {
+  if (typeof value === 'string') {
+    console.log(`String value: ${value}`)
+  } else if (typeof value === 'number') {
+    console.log(`Number value: ${value}`)
+  } else {
+    console.log(`Boolean value: ${value}`)
+  }
+}
