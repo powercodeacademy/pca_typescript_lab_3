1041b3ab617f5d73ce79f572c2b5349e60e04983
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-16 12:51:27 -0400

    Write validateInput function

--- src/section3_bonus_validateInput.ts
+++ src/section3_bonus_validateInput.ts
@@ -8,3 +8,17 @@
 // Use type narrowing (`typeof`, `in`) to handle each case.
 
 // Your code here 👇
+
+const validateInput = (input: string | number | object): boolean => {
+  if (typeof input === "string" && input.trim() !== "") {
+    return true
+  }
+  if (typeof input === "number" && input > 0) {
+    return true
+  }
+
+  if (typeof input === "object" && "valid" in input) {
+    return input.valid === true
+  }
+  return false
+}
